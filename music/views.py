632fc67e68fcb26877daf3225b43e7cc63df602d
최호준 <ensia96@choehojunui-MacBook-Pro.local>
2020-05-13 20:07:51 +0900

    deleted dummy, fixed .gitignore

--- music/views.py
+++ music/views.py
@@ -3,35 +3,6 @@ import os
 from django.http import StreamingHttpResponse
 from django.views import View
 
-<<<<<<< HEAD
-#from pydub import AudioSegment
-#
-#
-#class StreamView(View):
-#    def iteration(self, source, times):
-#        f = open(source, 'rb+')
-#        f.seek(int(times))
-#        while True:
-#            c = f.read(512)
-#            if c:
-#                yield c
-#            else:
-#                break
-#
-#    def get(self, request):
-#        audio = AudioSegment.from_file('roses.mp3')
-#        durations = int(len(audio) / 1000)
-#        size = os.path.getsize('roses.mp3')
-#        size_per_sec = size / durations
-#
-#        seconds = 0
-#        resp = StreamingHttpResponse(self.iteration('roses.mp3', size_per_sec * seconds), status=200,
-#                                     content_type='audio/mp3')
-#        resp['Cache-Control'] = 'no-cache'
-#        resp['Accept-Ranges'] = 'bytes'
-#
-#        return resp
-=======
 from pydub import AudioSegment
 
 
@@ -77,4 +48,3 @@ class StreamView(View):
                                      status=200, content_type='audio/mp3')
 
         return resp
->>>>>>> 28eecb65380ad467bb4f41f1eda919d009472a35
