e4bedf2126bff55977a1883f0a643afc959bd8ac
ikswary <ikswary@naver.com>
2020-05-13 19:53:01 +0900

    revise : iterator function to class

--- music/views.py
+++ music/views.py
@@ -6,16 +6,37 @@ from django.views import View
 from pydub import AudioSegment
 
 
+class RangeFileWrapper(object):
+    def __init__(self, filelike, blksize, length = 0):
+        self.filelike = filelike
+        self.blksize = blksize
+        self.remaining = length
+
+    def __close__(self):
+        if hasattr(self.filelike, 'close'):
+            self.filelike.close()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.remaining is None:
+            data = self.filelike.read(self.blksize)
+            if data:
+                return data
+            raise StopIteration()
+        else:
+            if self.remaining <= 0:
+                raise StopIteration()
+            data = self.filelike.read(min(self.remaining, self.blksize))
+            if not data:
+                raise StopIteration()
+            self.remaining -= len(data)
+            return data
+
+
 class StreamView(View):
     MILISECOND_TO_SECOND = 1000
-
-    def iteration(self, file, bytes_per_sec):
-        while True:
-            c = file.read(bytes_per_sec)
-            if c:
-                yield c
-            else:
-                break
 
     def get(self, request):
         audio = AudioSegment.from_mp3('roses.mp3')
@@ -23,7 +44,7 @@ class StreamView(View):
         size = os.path.getsize('roses.mp3')
         bytes_per_sec = int(size / playtime)
 
-        resp = StreamingHttpResponse(self.iteration(open('roses.mp3', 'rb+'), bytes_per_sec),
+        resp = StreamingHttpResponse(RangeFileWrapper(open('roses.mp3', 'rb+'), bytes_per_sec, size),
                                      status=200, content_type='audio/mp3')
 
         return resp
