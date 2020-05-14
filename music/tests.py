b0fd27a92618c0c93952a8daf67261c929d75000
ikswary <ikswary@naver.com>
2020-05-14 16:28:05 +0900

    update : testcode for StreamView

--- music/tests.py
+++ music/tests.py
@@ -2,16 +2,56 @@ from datetime import time
 
 from django.test import TestCase, Client
 
-from .models import Media
+from .models import (
+    Artist,
+    Thumbnail,
+    Type,
+    Collection,
+    Playlist,
+    Media
+)
 
 
 class StreamViewTest(TestCase):
     def setUp(self):
+        thumbnail = Thumbnail.objects.create(
+            url='test_url'
+        )
+
+        artist = Artist.objects.create(
+            name='test_artist',
+            url='test_url',
+            thumbnail=thumbnail,
+        )
+
+        type = Type.objects.create(
+            name='test_type'
+        )
+
+        collection = Collection.objects.create(
+            name='test_collection',
+            thumbnail=thumbnail,
+            type=type
+        )
+
+        playlist = Playlist.objects.create(
+            name='test_playlist',
+            artist=artist,
+            thumbnail=thumbnail,
+            type=type,
+            collection=collection
+        )
+
         Media(
             name='ra-mu-ne',
             length=time(minute=5, second=12),
             views=8320,
-            url='roses.mp3'
+            url='roses.mp3',
+            artist=artist,
+            thumbnail=thumbnail,
+            type=type,
+            collection=collection,
+            playlist=playlist
         ).save()
 
     def tearDown(self):
