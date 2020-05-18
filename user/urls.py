109f21c5936a75bcb414d9a7f45f0e20eb255a6c
ikswary <ikswary@naver.com>
2020-05-18 09:48:39 +0900

    add : login decorator

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import GoogleSignInView
+from .views import (
+    GoogleSignInView,
+    StorageView
+)
 
 urlpatterns = [
-    path('/signin', GoogleSignInView.as_view())
+    path('/signin', GoogleSignInView.as_view()),
+    path('/storage', StorageView.as_view())
 ]
